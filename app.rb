d6b01341e0b7103dd49d16df08522207b82ab7c7
Cheryl K <cherylkelson@icloud.com>
2019-02-25 21:38:36 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -7,19 +7,16 @@ class App < Sinatra::Base
 
 
 	get '/hello' do
-		"Hello World "
 		erb :hello
 	end
 
 
 	get '/goodbye' do
-		"Goodbye Joe"
 		erb :goodbye
 	end
 
 
 	get '/date' do
-		
 		erb :date
 	end
 
